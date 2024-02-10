2bf6d8c1c5d2b12da88ce7f2b66417200de65713
Olga K. <olga_kisilyova@yahoo.com>
2024-02-10 10:58:03 -0700

    Week11 changes

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,7 +1,6 @@
 package projects;
 
 import java.math.BigDecimal;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -17,13 +16,14 @@ public class ProjectsApp {
     private Scanner scanner = new Scanner(System.in);
     private ProjectService projectService = new ProjectService();
     private Project curProject;
-//@formatter:off
     private List<String> operations = List.of(
             "1) Add a project",
-    		"2) List projects",
-    		"3) Select a project"
+            "2) List projects",
+            "3) Select a project",
+            "4) Update project details",
+            "5) Delete a project"
     );
-//@formatter:on
+
     /**
      * Entry point for the Java application.
      */
@@ -46,54 +46,81 @@ public class ProjectsApp {
                     case -1:
                         done = exitMenu();
                         break;
-
                     case 1:
                         createProject();
                         break;
-                    
                     case 2:
-                    	listProjects();
-                    	break;
-                    	
+                        listProjects();
+                        break;
                     case 3:
-                    	selectProject();
-                    	break;
-
+                        selectProject();
+                        break;
+                    case 4:
+                        updateProjectDetails();
+                        break;
+                    case 5:
+                        deleteProject();
+                        break;
                     default:
                         System.out.println("\n" + selection + " is not a valid selection. Try again.");
                         break;
                 }
-            } 
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println("\nError: " + e + " Try again.");
             }
         }
     }
 
+    private void deleteProject() {
+        listProjects();
+
+        Integer projectId = getIntInput("Enter the ID of the project to delete");
+        if (Objects.nonNull(projectId)){
+            projectService.deleteProject(projectId);
+            System.out.println("Project " + projectId + " was successfully deleted.");
+            if (Objects.nonNull(curProject) && curProject.getProjectId().equals(projectId)) {
+                curProject = null;
+            }
+        }
+    }
+
+    private void updateProjectDetails() {
+        if (Objects.isNull(curProject)) {
+            System.out.println("\nPlease select a project.");
+            return;
+        }
+        String projectName = getStringInput("Enter the project name [" + curProject.getProjectName() + "]");
+        BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours [" + curProject.getEstimatedHours() + "]");
+        BigDecimal actualHours = getDecimalInput("Enter the actual hours [" + curProject.getActualHours() + "]");
+        Integer difficulty = getIntInput("Enter the project difficulty (1-5) [" + curProject.getDifficulty() + "]");
+        String notes = getStringInput("Enter the project name [" + curProject.getNotes() + "]");
+
+        Project project = new Project();
+        project.setProjectId(curProject.getProjectId());
+        project.setProjectName(Objects.isNull(projectName) ? curProject.getProjectName() : projectName);
+        project.setEstimatedHours(Objects.isNull(estimatedHours) ? curProject.getEstimatedHours() : estimatedHours);
+        project.setActualHours(Objects.isNull(actualHours) ? curProject.getActualHours() : actualHours);
+        project.setDifficulty(Objects.isNull(difficulty) ? curProject.getDifficulty() : difficulty);
+        project.setNotes(Objects.isNull(notes) ? curProject.getNotes() : notes);
+
+        projectService.modifyProjectDetails(project);
+        curProject = projectService.fetchProjectById(curProject.getProjectId());
+    }
+
     private void selectProject() {
-		listProjects();
-		Integer projectId = getIntInput("Enter a project ID to select a project");
-		
-		/* Unselect the current project. */
-		curProject = null;
-		
-		/* This will throw an exception if an invalid project ID is entered. */
-		curProject = projectService.fetchProjectById(projectId);
-		
-	}
+        listProjects();
+        Integer projectId = getIntInput("Enter a project ID to select a project");
 
-	private void listProjects() {
-		List<Project> projects = projectService.fetchAllProjects();
-		
-		System.out.println("\nProjects:");
-		
-		projects.forEach(project -> System.out.println(" " + project.getProjectId() + ": " + project.getProjectName()));
-    
-	}
+        curProject = null;
+        curProject = projectService.fetchProjectById(projectId);
+    }
 
-	/**
-     * Gather user input for a project row then call the project service to create the row.
-     */
+    private void listProjects() {
+        List<Project> projects = projectService.fetchAllProjects();
+        System.out.println("\nProjects:");
+        projects.forEach(project -> System.out.println(" " + project.getProjectId() + ": " + project.getProjectName()));
+    }
+
     private void createProject() {
         String projectName = getStringInput("Enter the project name");
         BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours to complete the project");
@@ -102,7 +129,6 @@ public class ProjectsApp {
         String notes = getStringInput("Enter the project notes");
 
         Project project = new Project();
-
         project.setProjectName(projectName);
         project.setEstimatedHours(estimatedHours);
         project.setActualHours(actualHours);
@@ -113,13 +139,6 @@ public class ProjectsApp {
         System.out.println("You have successfully created project: " + dbProject);
     }
 
-    /**
-     * Gets the user's input from the console and converts it to a BigDecimal.
-     *
-     * @param prompt The prompt to display on the console.
-     * @return A BigDecimal value if successful.
-     * @throws DbException Thrown if an error occurs converting the number to a BigDecimal.
-     */
     private BigDecimal getDecimalInput(String prompt) {
         String input = getStringInput(prompt);
 
@@ -128,47 +147,23 @@ public class ProjectsApp {
         }
 
         try {
-            // Create the BigDecimal object and set it to two decimal places (the scale).
             return new BigDecimal(input).setScale(2);
-        } 
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new DbException(input + " is not a valid decimal number.");
         }
     }
 
-    /**
-     * Called when the user wants to exit the application. It prints a message and returns
-     * {@code true} to terminate the app.
-     *
-     * @return {@code true}
-     */
     private boolean exitMenu() {
         System.out.println("Exiting the menu.");
         return true;
     }
 
-    /**
-     * This method prints the available menu selections. It then gets the user's menu selection from
-     * the console and converts it to an int.
-     *
-     * @return The menu selection as an int or -1 if nothing is selected.
-     */
     private int getUserSelection() {
         printOperations();
-
         Integer input = getIntInput("Enter a menu selection");
-
         return Objects.isNull(input) ? -1 : input;
     }
 
-    /**
-     * Prints a prompt on the console and then gets the user's input from the console. It then
-     * converts the input to an Integer.
-     *
-     * @param prompt The prompt to print.
-     * @return If the user enters nothing, {@code null} is returned. Otherwise, the input is converted to an Integer.
-     * @throws DbException Thrown if the input is not a valid Integer.
-     */
     private Integer getIntInput(String prompt) {
         String input = getStringInput(prompt);
 
@@ -183,39 +178,21 @@ public class ProjectsApp {
         }
     }
 
-    /**
-     * Prints a prompt on the console and then gets the user's input from the console. If the user
-     * enters nothing, {@code null} is returned. Otherwise, the trimmed input is returned.
-     *
-     * @param prompt The prompt to print.
-     * @return The user's input or {@code null}.
-     */
     private String getStringInput(String prompt) {
         System.out.print(prompt + ": ");
         String input = scanner.nextLine();
-
         return input.isBlank() ? null : input.trim();
     }
 
-    /**
-     * Print the menu selections, one per line.
-     */
     private void printOperations() {
         System.out.println("\nThese are the available selections. Press the Enter key to quit.");
-
-        // With Lambda expression
-        // operations.forEach(line -> System.out.println(" " + line));
-
-        // With enhanced for loop
         for (String line : operations) {
             System.out.println(" " + line);
         }
-        if(Objects.isNull(curProject)) {
-        	System.out.println("\nYou are not working with a project.");
+        if (Objects.isNull(curProject)) {
+            System.out.println("\nYou are not working with a project.");
+        } else {
+            System.out.println("\nYou are working with a project: " + curProject);
         }
-        else {
-        	System.out.println("\nYou are working with a project: " + curProject);
-        
     }
-   }
 }
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -37,8 +37,20 @@ public class ProjectService {
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException( 
 				"Project with project ID=" + projectId + " does not exist.")); 
 	}
-	 
-
+public void modifyProjectDetails(Project project) {
+	// TODO Auto-generated method stub
+	if(!projectDao.modifyProjectDetails(project)) {
+		throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+	}
+}
+/**
+ * @param projectId  	 
+ */
+public void deleteProject(Integer projectId) {
+	if(!projectDao.deleteProject(projectId)) {
+		throw new DbException("Project with ID=" + projectId + " does not exist.");
+	}
+}
 
 
 }
